851e66e52497e60932c30780b95f89f4cf0a8c62
Mitch R <olordhux+github@gmail.com>
2015-04-09 19:36:02 -0700

    tabs -> spaces

--- index.js
+++ index.js
@@ -12,58 +12,58 @@ var chatIDs = {};
 
 // Public chat socket handler
 io.on('connection', function(socket){
-		// IIFE returns closure for each socket
-		return (function(socket)	{
-					/* Declare var to store username for greeting, and initialize flag
-					 * to prevent simultaneous responses to multiple quick inputs */
-					var username, responseFlag = false;
-				
-					// One-off name registration for socket
-					socket.on('username', function(val) {
-						username = val;
-						chatIDs[username] = socket.id;
-						responseFlag = true;
-						setTimeout(function() {
-							socket.emit('typing');
-							setTimeout(function() {
-								io.to(socket.id).emit('chatMessage', haunt.greet(username));
-								responseFlag = false;
-							}, haunt.firstTyping())
-						}, haunt.firstPause());
-						
-					});
-				
-					// Message routing
-					socket.on('chatMessage', function(message){
-						if (responseFlag === false) {
-							responseFlag = true;
-							setTimeout(function() {
-								socket.emit('typing');
-								setTimeout(function() {
-									io.to(socket.id).emit('chatMessage', haunt.respond()); 
-									responseFlag = false;
-								}, haunt.responseTyping());
-							}, haunt.responsePause());
-						}
-					});
-				
-					// Listener for dashboard join
-					socket.on('joinRequest', function(id) {
-						socket.join(id);
-						console.log('Dashboard socket successfully joined room ID: ' + id);
-					});
-		
-			})
+    // IIFE returns closure for each socket
+    return (function(socket)  {
+          /* Declare var to store username for greeting, and initialize flag
+           * to prevent simultaneous responses to multiple quick inputs */
+          var username, responseFlag = false;
+        
+          // One-off name registration for socket
+          socket.on('username', function(val) {
+            username = val;
+            chatIDs[username] = socket.id;
+            responseFlag = true;
+            setTimeout(function() {
+              socket.emit('typing');
+              setTimeout(function() {
+                io.to(socket.id).emit('chatMessage', haunt.greet(username));
+                responseFlag = false;
+              }, haunt.firstTyping())
+            }, haunt.firstPause());
+            
+          });
+        
+          // Message routing
+          socket.on('chatMessage', function(message){
+            if (responseFlag === false) {
+              responseFlag = true;
+              setTimeout(function() {
+                socket.emit('typing');
+                setTimeout(function() {
+                  io.to(socket.id).emit('chatMessage', haunt.respond()); 
+                  responseFlag = false;
+                }, haunt.responseTyping());
+              }, haunt.responsePause());
+            }
+          });
+        
+          // Listener for dashboard join
+          socket.on('joinRequest', function(id) {
+            socket.join(id);
+            console.log('Dashboard socket successfully joined room ID: ' + id);
+          });
+    
+      })
 
 }());
 
 // Dashboard connection handler
 dashboard.on('connection', function(socket) {
-	// IIFE returns closure for each socket
-	return function(socket){
-		// Send object of ongoing chats to begin monitoring
-		socket.emit('chatIDSync', chatIDs);
-	}
+  // IIFE returns closure for each socket
+  return function(socket){
+    // Send object of ongoing chats to begin monitoring
+    socket.emit('chatIDSync', chatIDs);
+  }
 
 }());
 
@@ -71,13 +71,13 @@ dashboard.on('connection', function(socket) {
 
 // Serve index
 // app.get('/', function(req, res) {
-// 	res.sendFile(__dirname + '/public/index.html');
+//  res.sendFile(__dirname + '/public/index.html');
 // });
 
 // Dashboard route
 // app.route('/dashboard')
 // .get(function(req, res, next) {
-// 	res.sendFile(__dirname + '/dashboard/dashboard.html')
+//  res.sendFile(__dirname + '/dashboard/dashboard.html')
 // });
 
 
@@ -89,12 +89,12 @@ app.use('/dashboard', express.static(__dirname + '/dashboard', {index: "dashboar
 
 // Serve socket client file to browser when requested
 app.get('/socket.io.js', function (req, res, next) {
-	res.sendFile(__dirname + '/node_modules/socket.io/node_modules/socket.io-client/socket.io.js')
+  res.sendFile(__dirname + '/node_modules/socket.io/node_modules/socket.io-client/socket.io.js')
 });
 
 // Catch-all 404
 app.use(function(req, res) {
-	res.send('404');
+  res.send('404');
 });
 
 // Initialization
